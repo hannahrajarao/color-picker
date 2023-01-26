ea45b39d84b4d046f1dce1fc2ebf3077de27be69
hannahrajarao <hannahrajarao@gmail.com>
2023-01-25 23:09:03 -0500

    Display rgb color (as a whole number)

--- color-picker.js
+++ color-picker.js
@@ -3,8 +3,11 @@ function chooseColor(event, divName) {
     let y = event.offsetY;
     const colorDiv = document.getElementById(divName);
     if(divName === 'color')
-    console.log("("+x+", "+y+")");
-    colorDiv.style.backgroundColor = 'rgb(255,'+y*255/200+',0)';
+        console.log("("+x+", "+y+")");
+    const color_y = Math.floor(y*255/200)
+    const color_string = 'rgb(255,'+color_y+',0)'
+    colorDiv.innerHTML = color_string
+    colorDiv.style.backgroundColor = color_string;
 }
 function interpolateColor(percent) {
 
