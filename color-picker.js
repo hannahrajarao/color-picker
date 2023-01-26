dc83caed8b4a1e6e73cfaa2e83356c223ad5b49a
hannahrajarao <hannahrajarao@gmail.com>
2023-01-25 22:18:05 -0500

    Combine functions

--- color-picker.js
+++ color-picker.js
@@ -1,10 +1,10 @@
-function showLocation(event) {
+function chooseColor(event, divName) {
     let x = event.offsetX;
     let y = event.offsetY;
-    const colorDiv = document.getElementById('color');
-    colorDiv.innerHTML = "("+x+","+y+")";
+    const colorDiv = document.getElementById(divName);
+    console.log("("+x+", "+y+")");
     colorDiv.style.backgroundColor = 'rgb(255,'+y*255/200+',0)';
 }
 function interpolateColor(percent) {
-    
+
 }
