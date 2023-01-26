f3d575602f5a1a8afb23ed132b332c8ceb200e03
hannahrajarao <hannahrajarao@gmail.com>
2023-01-25 23:06:50 -0500

    Print coordinate in console when clicked

--- color-picker.js
+++ color-picker.js
@@ -2,6 +2,7 @@ function chooseColor(event, divName) {
     let x = event.offsetX;
     let y = event.offsetY;
     const colorDiv = document.getElementById(divName);
+    if(divName === 'color')
     console.log("("+x+", "+y+")");
     colorDiv.style.backgroundColor = 'rgb(255,'+y*255/200+',0)';
 }
