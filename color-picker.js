893f85c5a1fd9b92afa6105471741469dd56ce6a
hannahrajarao <hannahrajarao@gmail.com>
2023-01-26 00:10:26 -0500

    Change conversion factor after changing div size

--- color-picker.js
+++ color-picker.js
@@ -4,7 +4,7 @@ function chooseColor(event, divName) {
     const colorDiv = document.getElementById(divName);
     if(divName === 'color')
         console.log("("+x+", "+y+")");
-    const color_y = Math.floor(y*255/200)
+    const color_y = Math.floor(y*255/300);
     const color_string = 'rgb(255,'+color_y+',0)'
     colorDiv.innerHTML = color_string
     colorDiv.style.backgroundColor = color_string;
